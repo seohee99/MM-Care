17aff5b63a315141a59e29e6f943b95589674003
gaamjaa <49315208+gaamjaa@users.noreply.github.com>
2023-11-26 16:34:40 +0900

    [feat] 백엔드 환경설정 완료

---
+++ mmcare-be/src/main/java/com/service/spring/vo/Care.java
@@ -0,0 +1,17 @@
+package com.service.spring.vo;
+
+import lombok.Data;
+
+import java.sql.Timestamp;
+
+@Data
+public abstract class Care {
+    private long petSeq;
+    private int cnt;
+    private String nickname;
+    private String img;
+    private Timestamp date;
+    private String memo;
+
+    public Care() {}
+}
